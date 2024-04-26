798f6d80ccf50b3b0def1ed7900cc8f3d516b46a
JKorf <jankorf91@gmail.com>
2024-04-26 15:40:53 +0200

    wip

---
+++ OKX.Net/OKXExchange.cs
@@ -0,0 +1,25 @@
+﻿namespace OKX.Net
+{
+    /// <summary>
+    /// OKX exchange information and configuration
+    /// </summary>
+    public static class OKXExchange
+    {
+        /// <summary>
+        /// Exchange name
+        /// </summary>
+        public static string ExchangeName => "OKX";
+
+        /// <summary>
+        /// Url to the main website
+        /// </summary>
+        public static string Url { get; } = "https://www.okx.com";
+
+        /// <summary>
+        /// Urls to the API documentation
+        /// </summary>
+        public static string[] ApiDocsUrl { get; } = new[] {
+            "https://www.okx.com/docs-v5/en/"
+            };
+    }
+}
