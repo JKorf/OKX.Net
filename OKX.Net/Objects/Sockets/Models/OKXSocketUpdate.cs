7be214dc13616b851b0b103012a0bdc71673ca15
Jkorf <jankorf91@gmail.com>
2025-03-24 13:42:55 +0100

    Correctly set update type to snapshot for account and position socket updates

--- OKX.Net/Objects/Sockets/Models/OKXSocketUpdate.cs
+++ OKX.Net/Objects/Sockets/Models/OKXSocketUpdate.cs
@@ -7,4 +7,10 @@ internal class OKXSocketUpdate<T>
     public string? Action { get; set; } = string.Empty;
     [JsonPropertyName("data")]
     public T Data { get; set; } = default!;
+    [JsonPropertyName("curPage")]
+    public int? CurrentPage { get; set; }
+    [JsonPropertyName("lastPage")]
+    public bool? LastPage { get; set; }
+    [JsonPropertyName("eventType")]
+    public string? EventType { get; set; }
 }
