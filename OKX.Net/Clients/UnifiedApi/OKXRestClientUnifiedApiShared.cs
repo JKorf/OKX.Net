c8a7f51b6c4396fd0b3a1a7222b1825d29566fcb
JKorf <jankorf91@gmail.com>
2026-02-03 21:08:51 +0100

    wip

--- OKX.Net/Clients/UnifiedApi/OKXRestClientUnifiedApiShared.cs
+++ OKX.Net/Clients/UnifiedApi/OKXRestClientUnifiedApiShared.cs
@@ -132,7 +132,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return response;
         }
 
-        public async Task<ExchangeResult<SharedSymbol[]>> GetSpotSymbolsForBaseAssetAsync(string baseAsset)
+        async Task<ExchangeResult<SharedSymbol[]>> ISpotSymbolRestClient.GetSpotSymbolsForBaseAssetAsync(string baseAsset)
         {
             if (!ExchangeSymbolCache.HasCached(_topicSpotId))
             {
@@ -144,7 +144,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return new ExchangeResult<SharedSymbol[]>(Exchange, ExchangeSymbolCache.GetSymbolsForBaseAsset(_topicSpotId, baseAsset));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsSpotSymbolAsync(SharedSymbol symbol)
+        async Task<ExchangeResult<bool>> ISpotSymbolRestClient.SupportsSpotSymbolAsync(SharedSymbol symbol)
         {
             if (symbol.TradingMode != TradingMode.Spot)
                 throw new ArgumentException(nameof(symbol), "Only Spot symbols allowed");
@@ -159,7 +159,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return new ExchangeResult<bool>(Exchange, ExchangeSymbolCache.SupportsSymbol(_topicSpotId, symbol));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsSpotSymbolAsync(string symbolName)
+        async Task<ExchangeResult<bool>> ISpotSymbolRestClient.SupportsSpotSymbolAsync(string symbolName)
         {
             if (!ExchangeSymbolCache.HasCached(_topicSpotId))
             {
@@ -847,7 +847,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return response;
         }
 
-        public async Task<ExchangeResult<SharedSymbol[]>> GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
+        async Task<ExchangeResult<SharedSymbol[]>> IFuturesSymbolRestClient.GetFuturesSymbolsForBaseAssetAsync(string baseAsset)
         {
             if (!ExchangeSymbolCache.HasCached(_topicFuturesId))
             {
@@ -859,7 +859,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return new ExchangeResult<SharedSymbol[]>(Exchange, ExchangeSymbolCache.GetSymbolsForBaseAsset(_topicFuturesId, baseAsset));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(SharedSymbol symbol)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(SharedSymbol symbol)
         {
             if (symbol.TradingMode == TradingMode.Spot)
                 throw new ArgumentException(nameof(symbol), "Spot symbols not allowed");
@@ -874,7 +874,7 @@ namespace OKX.Net.Clients.UnifiedApi
             return new ExchangeResult<bool>(Exchange, ExchangeSymbolCache.SupportsSymbol(_topicFuturesId, symbol));
         }
 
-        public async Task<ExchangeResult<bool>> SupportsFuturesSymbolAsync(string symbolName)
+        async Task<ExchangeResult<bool>> IFuturesSymbolRestClient.SupportsFuturesSymbolAsync(string symbolName)
         {
             if (!ExchangeSymbolCache.HasCached(_topicFuturesId))
             {
