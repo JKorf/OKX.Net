4833d5d773887ed4a3e4f959c3fdebd88d1bf3e3
Jkorf <jankorf91@gmail.com>
2026-02-04 16:30:42 +0100

    wip

---
+++ OKX.Net/OKXUserDataTracker.cs
@@ -0,0 +1,63 @@
+﻿using OKX.Net.Interfaces.Clients;
+using CryptoExchange.Net.SharedApis;
+using CryptoExchange.Net.Trackers.UserData;
+using CryptoExchange.Net.Trackers.UserData.Objects;
+using Microsoft.Extensions.Logging;
+
+namespace OKX.Net
+{
+    /// <inheritdoc/>
+    public class OKXUserSpotDataTracker : UserSpotDataTracker
+    {
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public OKXUserSpotDataTracker(
+            ILogger<OKXUserSpotDataTracker> logger,
+            IOKXRestClient restClient,
+            IOKXSocketClient socketClient,
+            string? userIdentifier,
+            SpotUserDataTrackerConfig config) : base(
+                logger,
+                restClient.UnifiedApi.SharedClient,
+                null,
+                restClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                restClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                userIdentifier,
+                config)
+        {
+        }
+    }
+
+    /// <inheritdoc/>
+    public class OKXUserFuturesDataTracker : UserFuturesDataTracker
+    {
+        /// <inheritdoc/>
+        protected override bool WebsocketPositionUpdatesAreFullSnapshots => false;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        public OKXUserFuturesDataTracker(
+            ILogger<OKXUserFuturesDataTracker> logger,
+            IOKXRestClient restClient,
+            IOKXSocketClient socketClient,
+            string? userIdentifier,
+            FuturesUserDataTrackerConfig config) : base(logger,
+                restClient.UnifiedApi.SharedClient,
+                null,
+                restClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                restClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                socketClient.UnifiedApi.SharedClient,
+                userIdentifier,
+                config)
+        {
+        }
+    }
+}
